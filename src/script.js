8f5de39bfbc12754b812e22f1dd4c591e2946ad7
misProyectosPropios <inakifrutos@outlook.com>
2024-10-02 17:43:41 +0200

    CREATE class gameRoom with the 3 arrays and the names of the procedures and class player

--- src/script.js
+++ src/script.js
@@ -19,5 +19,43 @@ room.onPlayerJoin = function(player) {
     room.sendAnnouncement("Hola player")
   }
 
+class GameRoom {
+    constructor() {
+        this.red = new Array();
+        this.blue = new Array();
+        this.spect = new Array();
+    }
 
+    addPlayer(id) {
+
+    }
+
+    deletePlayer() {
+
+    }
+
+    movePlayer(id, team) {
+
+    }
+}
+
+class Player {
+    constructor(id, auth, name) {
+        this.id = id;
+        this.auth = auth;
+        this.name = name;
+    }
+
+    get id() {
+        return this.id;
+      }
+
+    get auth() {
+      return this.auth;
+    }
+    
+    get name() {
+      return this.name;
+    }
+}
 
