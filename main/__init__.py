a29bd362e8291ea37a3aa5c72d19f3c9a1cec198
misProyectosPropios <inakifrutos@outlook.com>
2024-10-22 20:36:54 +0200

    CHANGES TO DOCUMENTATION. Working the macros

--- main/__init__.py
+++ main/__init__.py
@@ -1,16 +1,13 @@
-def test_fn(x:float):
-    "Test function"
-    return x * 4 / 3
+#variables
+background_color: str = "#111"
+color: str= "FFF"
 
-def say_hello(s:str):
-    "Test procedure"
-    return "<i>Hello %s</i>" % s
-
+#Macros
 def define_env(env):
     "Declare environment for jinja2 templates for markdown"
 
-    for fn in [test_fn, say_hello]:
-        env.macro(fn)
+    #for fn in [test_fn, say_hello]:
+    #    env.macro(fn)
 
     @env.macro
     def saludar(s:str):
@@ -24,16 +21,59 @@ def define_env(env):
         return res
 
     @env.macro
-    def createDocumentationToFunction(name:str, use: str, params:tuple(str, str, str), pre: list(str), post: list(str)):
-        pass
+    def createDocumentationToFunction(name:str, use: str, params: list[tuple[str, str, str]], pre: list[str], post: list[str]) -> str:
+        res: str = "## Function {name} \n\n### Uses: \n".format(name=name)
+        res += "{use} \n\n### Params\n\n".format(use=use)
+
+        res += "|name  |type|description| \n"
+        res += "|----- |----|-------|\n"
+        for i in params:
+            res += "| {name} | {type} | {description} |\n".format(name=i[0], type=i[1], description=i[2]) 
+
+        #Write the precondition
+        res += "\n\n### Pre-conditions:\n\n"
+        for i in pre:
+            res += "    * {pre}".format(pre=i)
+
+        #Write the post-condition
+        res += "\n\n### Post-conditions\n"
+        for i in post:
+            res += "    * {post}".format(post=i)
+        print(res)
+        return res
 
     @env.macro
-    def createDocumentationToClass(className: str, use: str, params: tuple(str, str, str)):
-        pass
+    def createDocumentationToClass(className: str, use: str, params: list[tuple[str, str, str]]) -> str:
+        res: str = "## Class {name} \n\n### Uses: \n".format(name=className)
+        res += "{use} \n\n### Params\n\n".format(use=use)
+        res += "|name  |type|description| \n"
+        res += "|----- |----|-------|\n"
+        for i in params:
+            res += "| {name} | {type} | {description} |\n".format(name=i[0], type=i[1], description=i[2]) 
+        
+        res += "\n\n### Methods"
+        return res
 
     @env.macro
-    def createDocumentationToMethod(className: str, name:str, use: str, params:tuple(str, str, str), pre: list(str), post: list(str)):
-        pass
+    def createDocumentationToMethod(name:str, use: str, params: list[tuple[str, str, str]], pre: list[str], post: list[str]) -> str:
+        res: str = "#### {name} \n\n##### Uses: \n".format(name=name)
+        res += "{use} \n\n##### <span background-color=\"{back_color}\" color=\"{color}\" >Params </span>\n\n".format(use=use, back_color=background_color, color=color)
+        res += "|name  |type|description| \n"
+        res += "|----- |----|-------|\n"
+        for i in params:
+            res += "| {name} | {type} | {description} |\n".format(name=i[0], type=i[1], description=i[2])
+
+        #Write the precondition
+        res += "\n\n##### Pre-conditions:\n\n"
+        for i in pre:
+            res += "    * {pre} \n".format(pre=i)
+
+        #Write the post-condition
+        res += "\n\n##### Post-conditions\n"
+        for i in post:
+            res += "    * {post} \n".format(post=i) 
+        print(res)
+        return res
 
     
 
